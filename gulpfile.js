cecb4f3dce125b4b9bdbb13b80a85cc5742134f4
Ben Seager <github@benseager.co.uk>
2015-09-21 18:58:04 +0100

    Excluded the gulpfile from the coverage report

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 'use strict';
-
+/* istanbul ignore next */
 var gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	toMarkdown = require('gulp-jsdoc-to-markdown');
-	
+/* istanbul ignore next */	
 gulp.task('create-docs', function(){
 	return gulp.src('lib/**/*.js')
 		.pipe(concat('api.md'))
 		.pipe(toMarkdown())
 		.pipe(gulp.dest('./docs'));
 });
-
+/* istanbul ignore next */
 gulp.task('default', ['create-docs'], function(){});
